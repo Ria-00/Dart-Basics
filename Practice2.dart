c664f33b0404cedc27c6fc62f815a8b4b786b9cf
Riya Singh <ria_006@yahoo.com>
2024-01-20 12:07:53 +0530

    practice ques

---
+++ Practice2.dart
@@ -0,0 +1,16 @@
+void main(){ 
+
+  // Ques 1
+  // List<int> l=[123,456,789,432,654,521];
+  // List<int> li=[];
+  // for (int i=0;i<l.length;i++) {
+  //   String no=l[i].toString();
+  //   int n=int.parse(no.split('').reversed.join());
+  //   print(n);
+  //   li.add(n);
+  // }
+  // print(li);
+
+  // Ques 2
+  
+}
